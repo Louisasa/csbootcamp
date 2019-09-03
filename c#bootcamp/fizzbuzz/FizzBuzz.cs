ea925e2814f0b8dc58b41bf46430b7c124f728ae
Louisa Nash <Louisa.Nash@softwire.com>
2019-09-03 15:16:58 +0100

    step two for fb

--- c#bootcamp/fizzbuzz/FizzBuzz.cs
+++ c#bootcamp/fizzbuzz/FizzBuzz.cs
@@ -3,47 +3,63 @@ using System.Collections;
 
 public class FizzBuzz
 {
-    static private ArrayList checkIfDivisibleByThree(int index, ArrayList resultArrayList) {
+    static public void Main(String[] args)
+    {
+        for (int index = 1; index <= 100; index++)
+        {
+            ArrayList resultArrayList = new ArrayList();
+
+            checkIfDivisibleByThree(index, resultArrayList);
+            checkIfDivisibleByFive(index, resultArrayList);
+            checkIfDivisibleBySeven(index, resultArrayList);
+            checkIfDivisibleByEleven(index, resultArrayList);
+            checkIfDivisibleByThirteen(index, resultArrayList);
+            checkIfDivisibleBySeventeen(index, resultArrayList);
+
+            if (resultArrayList.Count > 0)
+            {
+                Console.WriteLine(string.Join("", resultArrayList));
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+
+    static private void checkIfDivisibleByThree(int index, ArrayList resultArrayList) {
         if (index % 3 == 0)
         {
             resultArrayList.Add("Fizz");
         }
-
-        return resultArrayList;
     }
 
-    static private ArrayList checkIfDivisibleByFive(int index, ArrayList resultArrayList)
+    static private void checkIfDivisibleByFive(int index, ArrayList resultArrayList)
     {
         if (index % 5 == 0)
         {
             resultArrayList.Add("Buzz");
         }
-
-        return resultArrayList;
     }
 
-    static private ArrayList checkIfDivisibleBySeven(int index, ArrayList resultArrayList)
+    static private void checkIfDivisibleBySeven(int index, ArrayList resultArrayList)
     {
         if (index % 7 == 0)
         {
             resultArrayList.Add("Bang");
         }
-
-        return resultArrayList;
     }
 
-    static private ArrayList checkIfDivisibleByEleven(int index, ArrayList resultArrayList)
+    static private void checkIfDivisibleByEleven(int index, ArrayList resultArrayList)
     {
         if (index % 11 == 0)
         {
             resultArrayList = new ArrayList();
             resultArrayList.Add("Bong");
         }
-
-        return resultArrayList;
     }
 
-    static private ArrayList checkIfDivisibleByThirteen(int index, ArrayList resultArrayList)
+    static private void checkIfDivisibleByThirteen(int index, ArrayList resultArrayList)
     {
         if (index % 13 == 0)
         {
@@ -64,42 +80,11 @@ public class FizzBuzz
                 resultArrayList.Add("Fezz");
             }
         }
-
-        return resultArrayList;
     }
 
-    static private ArrayList checkIfDivisibleBySeventeen(int index, ArrayList resultArrayList) {
+    static private void checkIfDivisibleBySeventeen(int index, ArrayList resultArrayList) {
         if (index % 17 == 0) {
             resultArrayList.Reverse();
-        }
-        return resultArrayList;
-    }
-
-    static public void Main(String[] args)
-    {
-        for (int index = 1; index <= 100; index++)
-        {
-            ArrayList resultArrayList = new ArrayList();
-
-            resultArrayList = checkIfDivisibleByThree(index, resultArrayList);
-            resultArrayList = checkIfDivisibleByFive(index, resultArrayList);
-            //resultArrayList = checkIfDivisibleBySeven(index, resultArrayList);
-            //resultArrayList = checkIfDivisibleByEleven(index, resultArrayList);
-            //resultArrayList = checkIfDivisibleByThirteen(index, resultArrayList);
-
-
-
-            if (resultArrayList.Count > 0)
-            {
-                String resultStringToPrint = "";
-                foreach (String result in resultArrayList) {
-                    resultStringToPrint += result;
-                }
-                Console.WriteLine(resultStringToPrint);
-            }
-            else {
-                Console.WriteLine(index);
-            }
         }
     }
 }
