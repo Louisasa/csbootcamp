d80297b619790bcb502ccc5430dd50401111eb66
Louisa Nash <Louisa.Nash@softwire.com>
2019-09-05 11:32:09 +0100

    alex approved step 3

--- c#bootcamp/supportbank/SupportBank/SupportApp.Console/BankFileReader.cs
+++ c#bootcamp/supportbank/SupportBank/SupportApp.Console/BankFileReader.cs
@@ -14,18 +14,9 @@ public class BankFileReader
     {
         if (fileName.EndsWith(".csv"))
         {
-            var reader = new StreamReader(File.OpenRead("C:/Users/LouNas/c#bootcamp/supportbank/SupportBank/SupportApp.Console/" + fileName));
+            CsvConvert csvConverter = new CsvConvert();
+            return csvConverter.CsvToTransactionList(new StreamReader(File.OpenRead("C:/Users/LouNas/c#bootcamp/supportbank/SupportBank/SupportApp.Console/" + fileName)));
 
-            var headers = GetHeaders(reader);
-
-            if (headers.Count>0)
-            {
-                return CreateTransactions(reader, headers);
-            }
-            else
-            {
-                return new List<Transaction>();
-            }
         }
         else if (fileName.EndsWith(".json"))
         {
@@ -35,36 +26,5 @@ public class BankFileReader
         {
             return null;
         }
-    }
-
-    private static Dictionary<string, int> GetHeaders(StreamReader reader)
-    {
-        var line = reader.ReadLine();
-        var values = line?.Split(',');
-        return Enumerable.Range(0, values.Length).ToDictionary(i => values[i], i => i);
-    }
-
-    private static List<Transaction> CreateTransactions(StreamReader reader, Dictionary<string, int> headers)
-    {
-        var transactionList = new List<Transaction>();
-        while (!reader.EndOfStream)
-        {
-            var line = reader.ReadLine();
-            var values = line.Split(',');
-
-            var date = values[headers["Date"]].ToLower();
-            var to = values[headers["To"]].ToLower();
-            var from = values[headers["From"]].ToLower();
-            var narrative = values[headers["Narrative"]].ToLower();
-
-            var successfullyParsed = decimal.TryParse(values[headers["Amount"]], out var amount);
-
-            if (successfullyParsed)
-            {
-                transactionList.Add(new Transaction(date, to, from, narrative, amount));
-            }
-        }
-
-        return transactionList;
     }
 }
---
+++ c#bootcamp/supportbank/SupportBank/SupportApp.Console/CsvConvert.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class CsvConvert
+{
+    public List<Transaction> csvToTransactionList(StreamReader reader)
+    {
+        var headers = GetHeaders(reader);
+
+        if (headers.Count > 0)
+        {
+            return CreateTransactions(reader, headers);
+        }
+        else
+        {
+            return new List<Transaction>();
+        }
+    }
+
+    private static Dictionary<string, int> GetHeaders(StreamReader reader)
+    {
+        var line = reader.ReadLine();
+        var values = line?.Split(',');
+        return Enumerable.Range(0, values.Length).ToDictionary(i => values[i], i => i);
+    }
+
+    private static List<Transaction> CreateTransactions(StreamReader reader, Dictionary<string, int> headers)
+    {
+        var transactionList = new List<Transaction>();
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(',');
+
+            var date = values[headers["Date"]].ToLower();
+            var to = values[headers["To"]].ToLower();
+            var from = values[headers["From"]].ToLower();
+            var narrative = values[headers["Narrative"]].ToLower();
+
+            var successfullyParsed = decimal.TryParse(values[headers["Amount"]], out var amount);
+
+            if (successfullyParsed)
+            {
+                transactionList.Add(new Transaction(date, to, from, narrative, amount));
+            }
+        }
+
+        return transactionList;
+    }
+}
